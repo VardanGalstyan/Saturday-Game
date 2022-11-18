0a65e5b7a1c7a91a5e2b20f863f5df01ad6ee8e6
Vardan Galstyan <vardan.galstjan@gmail.com>
2022-11-19 00:51:41 +0100

    refactor: removes history component and schema

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,60 +1,49 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import allPlayersReducer from './Reducers.js/playersReducers';
-import sessionsReducer from './Reducers.js/sessionsReducer';
-import userReducer from './Reducers.js/userReducer';
-import locationsReducer from './Reducers.js/locationsReducer';
-import historyReducer from './Reducers.js/historyReducer';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import allPlayersReducer from "./Reducers.js/playersReducers";
+import sessionsReducer from "./Reducers.js/sessionsReducer";
+import userReducer from "./Reducers.js/userReducer";
+import locationsReducer from "./Reducers.js/locationsReducer";
 
 export const initialState = {
+  players: {
+    data: [],
+    loading: false,
+    error: null,
+  },
 
-    players: {
-        data: [],
-        loading: false,
-        error: null
-    },
+  sessions: {
+    data: [],
+    loading: false,
+    error: null,
+  },
 
-    sessions: {
-        data: [],
-        loading: false,
-        error: null
-    },
+  user: {
+    data: {},
+    loading: false,
+    error: null,
+  },
 
-    user: {
-        data: {},
-        loading: false,
-        error: null
-    },
-
-    locations: {
-        data: [],
-        loading: false,
-        error: null
-    },
-
-    history: {
-        data: [],
-        loading: false,
-        error: null
-    },
-}
+  locations: {
+    data: [],
+    loading: false,
+    error: null,
+  },
+};
 
 const combinedReducers = combineReducers({
-    players: allPlayersReducer,
-    sessions: sessionsReducer,
-    user: userReducer,
-    locations: locationsReducer,
-    history: historyReducer
-
-
-})
+  players: allPlayersReducer,
+  sessions: sessionsReducer,
+  user: userReducer,
+  locations: locationsReducer,
+});
 
 export const store = createStore(
-    combinedReducers,
-    initialState,
-    process.env.REACT_APP_DEVELOPMENT ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(thunk)) : compose(applyMiddleware(thunk))
-)
+  combinedReducers,
+  initialState,
+  process.env.REACT_APP_DEVELOPMENT
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(thunk))
+    : compose(applyMiddleware(thunk))
+);
 
-
-
-export default store
+export default store;
--- src/services/sessions/schema.js
+++ src/services/sessions/schema.js
@@ -1,12 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const { Schema, model } = mongoose
+const { Schema, model } = mongoose;
 
 const LocationSchema = new Schema({
-    location_name: { type: String, required: true },
-})
+  location_name: { type: String, required: true },
+});
 
-const SessionSchema = new Schema({
+const SessionSchema = new Schema(
+  {
+    active_game: { type: Boolean, default: false, required: true },
     session_name: { type: String, required: true },
     session_date: { type: Date, required: true },
     session_time: { type: String, required: true },
@@ -14,14 +16,13 @@ const SessionSchema = new Schema({
     session_duration: { type: String, required: false },
     session_room: { type: String, required: false },
     playing: { type: Boolean, required: false, default: false },
-    players: [{ type: Schema.Types.ObjectId, ref: 'Player' }],
-    host: { type: Schema.Types.ObjectId, ref: 'Player' },
+    players: [{ type: Schema.Types.ObjectId, ref: "Player" }],
+    host: { type: Schema.Types.ObjectId, ref: "Player" },
     session_status: { type: Boolean, required: false },
     teams: [],
+  },
+  { timestamps: true }
+);
 
-},
-    { timestamps: true }
-)
-
-export const SessionModel = model("Session", SessionSchema)
-export const LocationModel = model("Location", LocationSchema)
+export const SessionModel = model("Session", SessionSchema);
+export const LocationModel = model("Location", LocationSchema);
